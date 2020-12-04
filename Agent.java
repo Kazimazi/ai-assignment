0f83ab44990e393eb4a24b2b3a8fa9ce0a6bc2a2
Attila Koszo <attila.koszo.official@gmail.com>
2020-12-04 15:05:03 +0100

    redo stuff

--- Agent.java
+++ Agent.java
@@ -8,7 +8,6 @@ import game.gmk.GomokuPlayer;
 public class Agent extends GomokuPlayer {
     /** Array for all kinda good actions might not be tho. */
     protected ArrayList<GomokuAction> actions = new ArrayList<GomokuAction>(); // make some kind of set for it
-
     private boolean LOG = false;
 
     /**
@@ -20,6 +19,11 @@ public class Agent extends GomokuPlayer {
      */
     public Agent(int color, int[][] board, Random random) {
         super(color, board, random);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                actions.add(new GomokuAction(i, j));
+            }
+        }
     }
 
     @Override
@@ -32,17 +36,17 @@ public class Agent extends GomokuPlayer {
                 i = random.nextInt(board.length);
                 j = random.nextInt(board[i].length);
             }
-            takeAction(i, j, color);
+            board[i][j] = color;
             return new GomokuAction(i, j);
         }
 
-        takeAction(prevAction.i, prevAction.j, 1 - color);
+        board[prevAction.i][prevAction.j] = 1 - color;
 
         GomokuAction action = null; // init best move
         int score = Integer.MIN_VALUE; // -Inf
-        for (GomokuAction a : actions) {
+        for (GomokuAction a : actions) { // NOTE decrease number of actions
             if (board[a.i][a.j] == GomokuGame.EMPTY) {
-                int s = negamax(board, 2, color);
+                int s = negamax(board, 3, color); // TODO change depth according to remainingTimes, or state of game
                 if (score < s) {
                     score = s;
                     action = a;
@@ -50,158 +54,37 @@ public class Agent extends GomokuPlayer {
             }
         }
 
-        takeAction(action.i, action.j, color);
+        board[action.i][action.j] = 1;
 
         return action;
     }
 
-    /**
-     * Takes a move but doesn't commit to it.
-     * @param a given move
-     * @param color player's color
-     * @return new board with given action.
-     */
-    protected int[][] tryAction(GomokuAction a, int color) {
-        int[][] newBoard = GomokuGame.copy(board);
-        newBoard[a.i][a.j] = color;
-        return newBoard;
+    // TODO implement
+    protected int scorePosition(int[][] board, int color) {
+        return 0;
     }
 
-    /**
-     * Change state of board and actions.
-     * @param i
-     * @param j
-     * @param color
-     */
-    protected void takeAction(int i, int j, int color) {
-        // color the board
-        board[i][j] = color;
-
-        // remove taken action from actions
-        if (!actions.isEmpty()) {
-            int index = -1;
-            //Log("Not empty action.");
-            //Log("ac " + i + " " + j);
-            for (GomokuAction action : actions) {
-                //Log("cy " + action);
-                if (action.i == i && action.j == j) {
-                    index = actions.indexOf(action);
-                }
-            }
-            Log(">Remove action: " + actions.get(index));
-            actions.remove(index);
-        }
-
-        int distance = 2;
-        for (int ni = i - distance; ni != i + distance + 1; ni++) {
-            int realI = (ni + board.length) % board.length;
-            for (int nj = j - distance; nj != j + distance + 1; nj++) {
-                int realJ = (nj + board[realI].length) % board[realI].length;
-                boolean isNotSameRowColumn = !(realI == i && realJ == j);
-                Log("Do we add? " + "realI: " + realI + " realJ: " + realJ + " > " + String.valueOf(isNotSameRowColumn));
-                // ha nem ugyan abban a sorban/oszlop parban vannak es
-                // ha az egy ures mezo a tablan es
-                // ha meg nincs benne az actionjainkben
-                if (isNotSameRowColumn && board[realI][realJ] == GomokuGame.EMPTY) {
-                    boolean isAlreadyInActions = false;
-                    for (GomokuAction a : actions) {
-                        if (a.i == realI && a.j == realJ)
-                            isAlreadyInActions = true;
-                    }
-                    if (!isAlreadyInActions) {
-                        GomokuAction action = new GomokuAction(realI, realJ);
-                        actions.add(action);
-                        Log(">Add action: " + action);
-                    }
-                }
-            }
-        }
-        Log("Actions: " + actions);
+    protected int[][] boardWithMove(int[][] board, GomokuAction action, int color) {
+        board[action.i][action.j] = color;
+        return board;
     }
 
-    protected ArrayList<int[][]> potentialBoards(int[][] board, int color) {
+    protected ArrayList<int[][]> getChildrenBoards(int[][] board, int color) {
         ArrayList<int[][]> boards = new ArrayList<>();
-        for (GomokuAction action : actions) {
-            if (board[action.i][action.j] == GomokuGame.EMPTY) {
-                board[action.i][action.j] = color; // do the move
-                boards.add(board);
-                board[action.i][action.j] = GomokuGame.EMPTY; // take the move back
-            }
-        };
+        for (GomokuAction action : actions) { // NOTE decrease number of actions
+            boards.add(boardWithMove(board, action, color));
+        }
         return boards;
     }
 
     protected int negamax(int[][] board, int depth, int player) {
         if (depth == 0 || GomokuGame.hasFive(board, color))
-            return color * scoreBoard(board, color) + scoreBoard(board, 1 - color);
+            return color * scorePosition(board, color);
         int value = Integer.MIN_VALUE;
-        for (int[][] child : potentialBoards(board, color)) {
+        for (int[][] child : getChildrenBoards(board, color)) {
             value = Integer.max(value, -negamax(child, depth - 1, -color));
         }
         return value;
-    }
-
-    protected int score(int[][] board, int color) {
-        int result = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                result = Integer.max(result, tryAction(new GomokuAction(i, j), color));
-                // DELME ne boarddal mukodjon hanem actionnal?
-            }
-        }
-        return result;
-    }
-
-    /**
-     * The score of a cell is the aggregated value of the any direction from
-     * there computed by {@link GreedyPlayer#countDirection(int, int, int, int, int)} function.
-     * @param i row of the cell
-     * @param j column of the cell
-     * @param c color
-     * @return aggregated neighbor score
-     */
-    protected int score(int i, int j, int c) {
-        // TODO check if line is sealed on one end, it would worth less
-        int result = 0;
-        // right up
-        result += 1 << countDirection(i, j, 1, -1, c);
-        // right
-        result += 1 << countDirection(i, j, 1, 0, c);
-        // right down
-        result += 1 << countDirection(i, j, 1, 1, c);
-        // down
-        result += 1 << countDirection(i, j, 0, 1, c);
-        // left down
-        result += 1 << countDirection(i, j, -1, 1, c);
-        // left
-        result += 1 << countDirection(i, j, -1, 0, c);
-        // left up
-        result += 1 << countDirection(i, j, -1, -1, c);
-        // up
-        result += 1 << countDirection(i, j, 0, -1, c);
-
-        //Log("score result: " + result);
-
-        return result;
-    }
-
-    /**
-     * Counts the number of consecutive cells from the specified start point at
-     * the specified direction belongs to the specified color.
-     * @param i row of start position
-     * @param j column of start position
-     * @param di row direction
-     * @param dj column direction
-     * @param c color
-     * @return number of consecutive cells
-     */
-    protected int countDirection(int i, int j, int di, int dj, int c) {
-        int ni = (i + board.length + di) % board.length;
-        int nj = (j + board[ni].length + dj) % board[ni].length;
-        if (board[ni][nj] != c) {
-            return 0;
-        }
-        return 1 + countDirection(ni, nj, di, dj, c);
     }
 
     private void Log(String string) {
@@ -209,3 +92,4 @@ public class Agent extends GomokuPlayer {
             System.out.println("~~~" + string);
     }
 }
+
